fcf78a648d23510e4b639bf0ab80897ab6775826
Sonia Delgado <soniaemi2003@hotmail.com>
2024-04-26 00:50:36 +0200

    Four commit

--- prueb.js
+++
@@ -1,9 +0,0 @@
-typeExtra=['kk','ee','yy','oo'];
-userExtraType='kk,ee';
-console.log (userExtraType.split(',').length)
-for (var i = 0; i < userExtraType.split(',').length; i++) {
-   // document.write(arrayDeCadenas[i] + " / ");
-    console.log(typeExtra.includes(userExtraType.split(',')[i]))
-  }
-
-  
